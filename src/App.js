3000182426761014ddb5d34fb5c7350c4f0e6242
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2021-07-17 18:21:59 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import BascetWithGoods from "./Components/BasketWithGoods";
+
+export default function App() {
+  return (
+    <div>
+      <BascetWithGoods />
+    </div>
+  );
+}
